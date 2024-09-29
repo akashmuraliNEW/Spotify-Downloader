c46447c884716fd1e7e6d8d6c36f7a7e2bd1fb68
SMRITHI KRISHNAN.V.K <akashmurali013@gmail.com>
2024-09-29 22:53:59 +0530

    Update greetings.py

--- mbot/plugins/greetings.py
+++ mbot/plugins/greetings.py
@@ -55,10 +55,10 @@ async def start(client,message):
         await Data.insert_one({'id': user_id}) 
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention, message.from_user.username, bot_name))
     
-    if LOG_GROUP:
+    # if LOG_GROUP:
 
-        invite_link = await client.create_chat_invite_link(chat_id=(int(LOG_GROUP) if str(LOG_GROUP).startswith("-100") else LOG_GROUP))
-        reply_markup.append([InlineKeyboardButton("LOG Channel", url=invite_link.invite_link)])
+    #     invite_link = await client.create_chat_invite_link(chat_id=(int(LOG_GROUP) if str(LOG_GROUP).startswith("-100") else LOG_GROUP))
+    #     reply_markup.append([InlineKeyboardButton("LOG Channel", url=invite_link.invite_link)])
     if message.chat.type != "private" and message.chat.id not in AUTH_CHATS and message.from_user.id not in SUDO_USERS:
         return await message.reply_text("This Bot Will Not Work In Groups Unless It's Authorized.",
                     reply_markup=InlineKeyboardMarkup(reply_markup))
